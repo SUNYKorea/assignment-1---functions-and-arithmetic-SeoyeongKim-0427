ba1ffe21f7ff183816658a1de8ce9c3ced1d3b80
SeoyeongKim-0427 <seoyeongkim@stonybrook.edu>
2023-03-28 00:17:52 +0900

    All exercises completed

--- Homework_1.py
+++ Homework_1.py
@@ -1,17 +1,51 @@
-# Name:
-# SBUID: 
+# Name: Seoyeong Kim
+# SBUID: 115933295
 
 # Remove the ellipses (...) when writing your solutions.
-
+import math
 # ---------------------------- Exercise I ---------------------------------------
 # ----------------- Convert Fahrenheit to Celsius -------------------------------
 # TODO: Complete the implementation of fahrenheit2celsius () and what_to_wear(). 
 
-def fahrenheit2celsius(fahrenheit): 
-   ...
+def fahrenheit2celsius(fahrenheit):
+
+    try:
+        float(fahrenheit)
+
+    except ValueError:
+        print("Invalid input; the temperature is now 40 which is 4.4 degrees Celsius.")
+        return (5/9)*(40 - 32)
+
+    else:
+        return int((5/9)*(float(fahrenheit) - 32))
 
 def what_to_wear(celsius):
-   ...
+   clothes = ['puffy jacket', 'scarf', 'sweater', 'light jacket', 'T-shirt', '...did not think of this case.']
+
+   if(celsius < -10):
+       wear_this_clothe = clothes[0]
+    
+   elif(-10 < celsius < 0):
+       wear_this_clothe = clothes[1]
+    
+   elif(0 < celsius < 10):
+       wear_this_clothe = clothes[2]
+       
+   elif(10 < celsius < 20):
+       wear_this_clothe = clothes[3]
+       
+   elif(celsius > 20):
+       wear_this_clothe = clothes[4]
+    
+   else:
+       wear_this_clothe = clothes[5]
+       #The purpose of this code is for Celsius degrees exactky at 10, 20, etc.
+       #because the clothes for such degrees are not defined.
+    
+   print("The temperature is ", celsius, " degrees Celsius.")
+   print("You should wear a", wear_this_clothe, "\n")
+       
+       
 
 # ---------------------------- Exercise II --------------------------------------
 # ----------------- Area and perimeter of a triangle  ---------------------------
@@ -19,13 +53,16 @@ def what_to_wear(celsius):
 # compute_triangle_perimeter from scratch  
 
 def shoelace_triangle_area(x1, y1, x2, y2, x3, y3):
-    ...
+    return abs(1/2 * ( ((x1 * y2) + (x2 * y3) + (x3 * y1)) - ((x1 * y3) + (x2 * y1) + (x3 * y2)) ))
 
 def euclidean_distance(x1, y1, x2, y2):
-    ...
+    return ( ((x1 - x2) ** 2) + ((y1 - y2) ** 2) ) ** 0.5
 
 def compute_triangle_perimeter(x1, y1, x2, y2, x3, y3):
-    ...
+    s1 = euclidean_distance(x1, y1, x2, y2)
+    s2 = euclidean_distance(x2, y2, x3, y3)
+    s3 = euclidean_distance(x3, y3, x1, y1)
+    return round(s1 + s2 + s3, 4)
 
 
 # ---------------------------- Exercise III -------------------------------------
@@ -34,13 +71,13 @@ def compute_triangle_perimeter(x1, y1, x2, y2, x3, y3):
 
 
 def deg2rad(deg):
-    ...
+    return deg*(math.pi/180)
 
 def apothem(number_sides, length_side):
-   ...
+    return length_side / 2*math.tan(180/number_sides)
 
 def polygon_area(number_sides, length_side):
-   ...
+   return round(number_sides * length_side * apothem(number_sides, length_side), 4)
 
 
 # ---------------------------- Test -------------------------------------
@@ -48,12 +85,12 @@ def polygon_area(number_sides, length_side):
 # your grade. You may freely modify the following codes.
 
 # Exercise 1 test
-fahrenheit = 40
+fahrenheit = 50
 what_to_wear(fahrenheit2celsius(fahrenheit))
 
 # Exercise 2 test
 x1, x2, x3, y1, y2, y3 = -4, -5, 3, -4, 5, -3 # declaration of the vertices of the triangle
-area = shoelace_triangle_area(x1, y1, x2, y2, x3, y3)
+area = shoelace_triangle_area(x1, x2, x3, y1, y2, y3)
 perimeter = compute_triangle_perimeter(x1, y1, x2, y2, x3, y3)
 print("The area of the triangle is : " + str(area) + " , its perimeter is : " + str(perimeter) )
 
